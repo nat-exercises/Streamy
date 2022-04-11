3f6cd76e4fa124d21f75ed2c92ff38080e32a0f0
natacha <nat_bartu@hotmail.com>
2022-04-11 14:02:05 +1000

    set sign in and sign out with google api

--- src/components/autorization/GoogleAuth.js
+++ src/components/autorization/GoogleAuth.js
@@ -3,17 +3,38 @@ import React, { Component } from "react";
 const CLIENT_ID = process.env.REACT_APP_STREAMY_CLIENT_ID;
 
 class GoogleAuth extends Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId: CLIENT_ID,
-        scope: "email",
-      });
+      window.gapi.client
+        .init({
+          clientId: CLIENT_ID,
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
     });
+  }
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if we are signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>I am signed in!</div>;
+    } else {
+      return <div>I am not signed in</div>;
+    }
   }
 
   render() {
-    return <div>GoogleAuth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
