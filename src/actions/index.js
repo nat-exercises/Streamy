22a59fdbd8157d9d58e6fc8f0723b6b9d991a0b1
natacha <nat_bartu@hotmail.com>
2022-04-12 12:08:12 +1000

    wire the redux-thunk into the index file and import into streamCreate component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 import { SIGN_IN, SIGN_OUT } from "./types";
+import streams from "../apis/streams";
+
 export const signIn = (userId) => {
   return {
     type: SIGN_IN,
@@ -10,4 +12,8 @@ export const signOut = () => {
   return {
     type: SIGN_OUT,
   };
+};
+
+export const createStream = (formValues) => async (dispatch) => {
+  streams.post("/streams", formValues);
 };
