1d4405a3b9b4c0ec14a0fc9aab9a43447dd8182c
natacha <nat_bartu@hotmail.com>
2022-04-26 11:47:07 +1000

    fetch individual stream on edit page

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import streams from "../apis/streams";
+import history from "../history/history";
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -7,7 +9,6 @@ import {
   FETCH_STREAM,
   DELETE_STREAM,
 } from "./types";
-import streams from "../apis/streams";
 
 export const signIn = (userId) => {
   return {
@@ -27,6 +28,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
   const response = await streams.post("/streams", { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push("/");
 };
 
 export const fetchStreams = () => async (dispatch) => {
