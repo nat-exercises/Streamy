cde4ddb1005f2b35acbda40f91845280ffe20cbe
natacha <nat_bartu@hotmail.com>
2022-04-27 14:05:27 +1000

    create a modal and set delete stream using modal

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Modal from "../../modal/Modal";
+import history from "../../history/history";
 
 const StreamDelete = () => {
+  const actions = (
+    <>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </>
+  );
+
   return (
     <div>
-      <div>StreamDelete</div>
-      <Modal />
+      <div>Stream Delete</div>
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+        onDismiss={() => history.push("/")}
+      />
     </div>
   );
 };
